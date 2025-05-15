0e5f4ee786fcce323f7d839553fa55299c1b2e77
lucash <lucasAdmin@gmail.com>
2025-05-15 14:51:04 -0300

    feat: implementar persistência de tarefas com localStorage, usando useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
 
-const [tarefas, setTarefas] = useState([
-  'Pagar a conta de luz',
-  'Estudar React Hooks'
-]);
-const [nome, setNome] = useState('Lucas');
+  const [tarefas, setTarefas] = useState([]);
+  const [input, setInput] = useState('');
+
+
+  useEffect(() => {  // Corrigido: Estava faltando o '=' após '>'
+    const tarefasStorage = localStorage.getItem('tarefas');
+    
+    if(tarefasStorage){
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+  }, []);
+
+
+  useEffect(() => {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+  }, [tarefas])
+
+
+  function handleAdd(){
+    setTarefas([...tarefas, input])
+    setInput('');
+  }
 
   return (
     <div>
@@ -15,7 +32,8 @@ const [nome, setNome] = useState('Lucas');
             <li key={tarefa}>{tarefa}</li>
           ))}
         </ul>
-        <h1>{nome}</h1>
+        <input type="text" value={input} onChange={e => setInput(e.target.value)}></input>
+        <button type="button" onClick={handleAdd}>Adicionar</button>
     </div>
   );
 }
